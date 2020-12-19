f249b5f0653dc23aa85d98cf5f77a892cd4e9189
sontaj <kontakt@jakubsonta.pl>
2020-12-19 06:44:15 +0100

    Parse JSON in web version

--- trollfactory.py
+++ trollfactory.py
@@ -25,10 +25,9 @@ def print_properties(properties):
         for key in properties[property]:
             output('    ' + key + ': ' + str(properties[property][key]))
 
-def generate(language, sex, output_type):
-    output_lines = []
+def generate(language, sex):
     properties = load_props()
-    if language == 'polish': country_code = 'PL'
+    country_code = {'polish': 'PL'}[language]
     properties_static = {'language': {'language': language, 'country_code': country_code}, 'sex': {'sex': sex}}
 
     while len(properties) > 0:
@@ -41,17 +40,13 @@ def generate(language, sex, output_type):
                     if not dependency.lower() in properties_static.keys():
                         missing_dependencies = True
             if (missing_dependencies): continue
-            output_lines.append('Prop name: {}'.format(prop_name))
             prop_attrs = prop_class.generate(properties_static)
-            for key, value in sorted(prop_attrs.items()): output_lines.append('    {}: {}'.format(key, value))
             properties_static[prop_name] = prop_attrs
             properties.remove(prop_name)
-            output_lines.append('End prop')
-    if output_type == 'normal': return(output_lines)
-    elif output_type == 'json': return(dumps(properties_static))
+    return(dumps(properties_static))
 
 if __name__ == '__main__':
     output('Starting TrollFactory..\n')
     for _ in range(args.amount):
         properties = load_props()
-        print_properties(loads(generate('polish', sex, 'json')))
+        print_properties(loads(generate('polish', sex)))
