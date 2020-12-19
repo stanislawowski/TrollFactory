6ba3d86166a579c1e66f949ff7e9cbac60573617
0xA7404A <nyaurora@disroot.org>
2020-12-19 06:08:09 +0100

    Merge generation functions

--- trollfactory.py
+++ trollfactory.py
@@ -2,7 +2,7 @@ from os.path import basename, isfile, join
 from datetime import datetime
 from glob import glob
 from argparse import ArgumentParser
-from json import dumps
+from json import dumps, loads
 
 parser = ArgumentParser(description='Fake identities generator.')
 parser.add_argument('--amount', dest='amount', type=int, default=1)
@@ -19,39 +19,13 @@ def load_props(props = None):
         __all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
     return __all__
 
-def generate():
-    language = 'polish'
-    country_code = 'PL'
+def print_properties(properties):
+    for property in properties:
+        output('Prop name: ' + property)
+        for key in properties[property]:
+            output('    ' + key + ': ' + str(properties[property][key]))
 
-    print('\n')
-    output('Generating identity [{}, {}, {}]..'.format('no preset', language.capitalize(), sex))
-    properties_static = {'language': {'language': language, 'country_code': country_code}, 'sex': {'sex': sex}}
-
-    while (len(properties) > 0):
-        for prop_name in properties:
-            prop = __import__('props.'+prop_name)
-
-            prop_class = getattr(getattr(prop, prop_name), prop_name.capitalize())
-
-            missing_dependencies = False
-            if hasattr(prop_class, 'dependencies'):
-                for dependency in prop_class.dependencies:
-                    if not dependency.lower() in properties_static.keys():
-                        missing_dependencies = True
-            if (missing_dependencies):
-                continue
-
-            output('Prop name: {}'.format(prop_name))
-
-            prop_attrs = prop_class.generate(properties_static)
-
-            for key, value in sorted(prop_attrs.items()):
-                output('    {}: {}'.format(key, value))
-
-            properties_static[prop_name] = prop_attrs
-            properties.remove(prop_name)
-
-def generate_web(language, sex, output_type):
+def generate(language, sex, output_type):
     output_lines = []
     properties = load_props()
     if language == 'polish': country_code = 'PL'
@@ -77,7 +51,7 @@ def generate_web(language, sex, output_type):
     elif output_type == 'json': return(dumps(properties_static))
 
 if __name__ == '__main__':
-    output('Starting TrollFactory..')
+    output('Starting TrollFactory..\n')
     for _ in range(args.amount):
         properties = load_props()
-        generate()
+        print_properties(loads(generate('polish', sex, 'json')))
