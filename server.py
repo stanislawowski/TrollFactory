c7e801de66d9bb50ebd5f096239ee0c80e0fa73c
0xA7404A <nyaurora@disroot.org>
2020-12-19 06:44:15 +0100

    Parse JSON in web version

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from trollfactory import generate
+from json import loads
 app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
@@ -8,12 +9,12 @@ def index():
 
 @app.route('/output/<language>/<sex>')
 def output(language, sex):
-	generated = generate(language, sex, 'normal')
+	generated = loads(generate(language, sex))
 	return render_template('/output.html', generated=generated)
 
 @app.route('/json/<language>/<sex>')
 def json(language, sex):
-    return generate(language, sex, 'json')
+    return generate(language, sex)
 
 if __name__ == '__main__':
     app.run(port=2137)
