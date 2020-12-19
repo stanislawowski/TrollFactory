8ed6fb8ed6ecee7532da42eee850ba5b0f5cfb5e
sontaj <kontakt@jakubsonta.pl>
2020-12-19 06:08:09 +0100

    Merge generation functions

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from trollfactory import generate_web
+from trollfactory import generate
 app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
@@ -8,12 +8,12 @@ def index():
 
 @app.route('/output/<language>/<sex>')
 def output(language, sex):
-	generated = generate_web(language, sex, 'normal')
+	generated = generate(language, sex, 'normal')
 	return render_template('/output.html', generated=generated)
 
 @app.route('/json/<language>/<sex>')
 def json(language, sex):
-    return generate_web(language, sex, 'json')
+    return generate(language, sex, 'json')
 
 if __name__ == '__main__':
     app.run(port=2137)
