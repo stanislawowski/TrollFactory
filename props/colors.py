9cbc1cd54377680bed1229fbf5edd02d47540d06
sontaj <kontakt@jakubsonta.pl>
2020-12-19 09:50:28 +0100

    Pretty prop titles

--- props/colors.py
+++ props/colors.py
@@ -3,6 +3,7 @@ from random import choice
 class Colors:
     def generate(properties):
         return {
+            'prop_title': 'Colors',
             'favourite': choice(['black', 'white', 'grey', 'red', 'blue', 'navy', 'green', 'pink', 'white', 'purple', 'yellow', 'green', 'orange']),
             'hair': choice(['blonde', 'brown', 'black', 'auburn', 'red']),
             'eyes': choice(['amber', 'blue', 'brown', 'gray', 'green', 'hazel'])
--- props/document_id.py
+++ props/document_id.py
@@ -11,5 +11,6 @@ class Document_id:
       chcksum = (ord(id_number[0])-55) * 7 + (ord(id_number[1])-55) * 3 + (ord(id_number[2])-55) + id_number[3] * 9 + id_number[4] * 7 + id_number[5] * 3 + id_number[6] + id_number[7] * 7 + id_number[8] * 3
 
       return {
+          'prop_title': 'Document - ID',
           'id_number': ''.join(map(str, id_number))
       }
