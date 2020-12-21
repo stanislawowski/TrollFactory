99e9d30e78435ae4747b88081fb6452f64daf6c9
0xA7404A <nyaurora@disroot.org>
2020-12-21 06:49:26 +0100

    Change US value to english_us

--- props/address.py
+++ props/address.py
@@ -5,7 +5,7 @@ class Address:
     depedencies = ["language"]
 
     def generate(properties):
-        if properties['language']['language'] == 'english':
+        if properties['language']['language'] == 'english_us':
             return {'prop_title': 'Address', 'address': 'Not available in US yet!'}
         region = choice(load(open('langs/' + properties['language']['language'] + '/cities.json')))
 
