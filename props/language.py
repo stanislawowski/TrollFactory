103e74b14ac4910abd58123084a4a94af8c4c190
pzdolski <dante383@protonmail.com>
2021-02-28 22:16:26 +0100

    preparing for setting props manually

--- props/language.py
+++ props/language.py
@@ -1,7 +1,13 @@
+from random import choice 
+from glob import glob 
+
 class Language:
-    def generate(properties):
-        return {
-            'prop_title': 'Language',
-            'language': properties['language']['language'],
-            'country_code': properties['language']['country_code']
-        }
+	def generate(properties):
+		language = properties['language']['language'] if 'language' in properties.get('language', {}) else choice(glob("langs/*/")).split('/')[1]
+		country_code = properties['language']['country_code'] if 'country_code' in properties.get('language', {}) else {"english_us": 'US', "polish": 'PL'}[language]
+
+		return {
+		'prop_title': 'Language',
+		'language': language,
+		'country_code': country_code
+		}
