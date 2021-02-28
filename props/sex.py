b69b1d6d848c16273a44309683c71c9574891d50
pzdolski <dante383@protonmail.com>
2021-02-28 22:16:26 +0100

    preparing for setting props manually

--- props/sex.py
+++ props/sex.py
@@ -1,6 +1,8 @@
+from random import choice 
+
 class Sex:
     def generate(properties):
         return {
             'prop_title': 'Sex',
-            'sex': properties['sex']['sex']
+            'sex': properties['sex']['sex'] if 'sex' in properties.get('sex', {}) else choice(['male', 'female']),
         }
