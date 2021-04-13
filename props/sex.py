e360011eb49f9d3dcd7f9a359261aa139c5fee5b
stanislawowski <office@stanislawowski.pl>
2021-04-13 23:22:01 +0200

    I hate my life

--- props/sex.py
+++ props/sex.py
@@ -1,8 +1,6 @@
-from random import choice
-
 class Sex:
     def generate(properties):
         return {
             'prop_title': 'Sex',
-            'sex': properties['sex']['sex'] if 'sex' in properties.get('sex', {}) else choice(['male', 'female']),
+            'sex': properties['sex']['sex']
         }
