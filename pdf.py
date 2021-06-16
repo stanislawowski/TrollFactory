4fce9fdaded093a137722a65f3df67fa259a3f92
nyarr <nyaurora@disroot.org>
2021-06-16 18:57:37 +0200

    Unicode support

--- pdf.py
+++ pdf.py
@@ -1,7 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
 from fpdf import FPDF
-
-def fix_enc(s):
-    return s.encode('latin-1', 'ignore').decode('latin-1')
 
 def fix_title(s):
     return s.replace('_', ' ').capitalize()
@@ -9,18 +8,20 @@ def fix_title(s):
 def generate_pdf(personality, id):
     pdf = FPDF(format='A4')
     pdf.add_page()
+    pdf.add_font('PTSans', '', 'static/fonts/PTSans-Regular.ttf', uni=True)
+    pdf.add_font('PTSans', 'B', 'static/fonts/PTSans-Bold.ttf', uni=True)
     
     for prop_name in personality.keys():
-        pdf.set_font('Arial', 'B', 16)
+        pdf.set_font('PTSans', 'B', 16)
         pdf.cell(60, 10, personality[prop_name]['prop_title'], 0, 1, 'C')
 
-        pdf.set_font('Arial', '', 16)
+        pdf.set_font('PTSans', '', 16)
         for prop_entry in personality[prop_name].keys():
             if prop_entry == 'user_agent':
                 for part in personality[prop_name][prop_entry]:
-                    pdf.cell(0, 10, fix_enc(str(part)), 0, 1)
+                    pdf.cell(0, 10, str(part), 0, 1)
             elif prop_entry != 'prop_title':
-                line = fix_enc(fix_title(prop_entry)+': '+str(personality[prop_name][prop_entry]))
+                line = fix_title(prop_entry)+': '+str(personality[prop_name][prop_entry])
                 pdf.cell(0, 10, line, 0, 1)
 
         pdf.ln(5)
