b64aed2dd21fb1a53d4fdf14d1941853ec99996d
stanislawowski <office@stanislawowski.pl>
2021-08-04 23:11:13 +0200

    Renamed 'sex' prop to 'gender'

---
+++ props/gender.py
@@ -0,0 +1,6 @@
+class Gender:
+    def generate(properties):
+        return {
+            'prop_title': 'Gender',
+            'gender': properties['gender']['gender']
+        }
--- trollfactory.py
+++ trollfactory.py
@@ -11,13 +11,13 @@ TROLLFACTORY_VERSION = "2.0.0"
 
 parser = ArgumentParser(description='Fake identities generator.')
 parser.add_argument('--amount', dest='amount', type=int, default=1)
-parser.add_argument('--sex', dest='sex', type=str, default='male')
+parser.add_argument('--gender', dest='gender', type=str, default='male')
 parser.add_argument('--dataset', dest='dataset', type=str, default='polish')
 parser.add_argument('-o', '--output', dest='file', type=str)
 parser.add_argument('--no-stdout', dest='stdout', action='store_false')
 parser.add_argument('-v', '--version', action='store_true')
 args = parser.parse_args()
-sex = args.sex
+gender = args.gender
 dataset = args.dataset
 
 def output(text):
@@ -41,10 +41,10 @@ def print_properties(properties):
             if (properties[prop][key] != None):
                 output('    ' + key + ': ' + str(properties[prop][key]))
 
-def generate(dataset, sex):
+def generate(dataset, gender):
     properties = load_props()
     country_code = {'polish': 'PL', 'english_us': 'US'}[dataset]
-    properties_static = {'language': {'language': dataset, 'country_code': country_code}, 'sex': {'sex': sex}}
+    properties_static = {'language': {'language': dataset, 'country_code': country_code}, 'gender': {'gender': gender}}
 
     while len(properties) > 0:
         for prop_name in properties:
@@ -53,18 +53,18 @@ def generate(dataset, sex):
             prop_class = getattr(getattr(prop, prop_name), prop_name.capitalize())
 
             missing_dependencies = False
-            
+
             if hasattr(prop_class, 'dependencies'):
                 for dependency in prop_class.dependencies:
                     if not dependency.lower() in properties_static.keys():
                         missing_dependencies = True
             if (missing_dependencies): continue
-            
+
             prop_attrs = prop_class.generate(properties_static)
-            
+
             properties_static[prop_name] = prop_attrs
             properties.remove(prop_name)
-    
+
     return properties_static
 
 if __name__ == '__main__':
@@ -74,7 +74,7 @@ if __name__ == '__main__':
     output('Starting TrollFactory..\n')
     for _ in range(args.amount):
         properties = load_props()
-        generated = generate(dataset, sex)
+        generated = generate(dataset, gender)
 
         if args.stdout:
             print_properties(generated)
