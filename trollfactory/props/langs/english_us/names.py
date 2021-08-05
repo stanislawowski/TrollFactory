913ed174614e03ad3ae8fa574451f7c957b3c8b5
stanislawowski <office@stanislawowski.pl>
2021-08-05 15:35:14 +0200

    Code quality improvements

--- trollfactory/props/langs/english_us/names.py
+++ trollfactory/props/langs/english_us/names.py
@@ -1,31 +1,31 @@
 import csv, json
 
 def main ():
-	names_women = []
-	names_men = []
+    names_women = []
+    names_men = []
 
-	total_women = 0
-	total_men = 0
+    total_women = 0
+    total_men = 0
 
-	with open('names.csv', newline='') as women_csv:
-		reader = csv.reader(women_csv)
-		for row in reader:
-			if (row[1] == 'F'):
-				names_women.append([row[0].lower().capitalize(), int(row[2])])
-				total_women += int(row[2])
-			else:
-				names_men.append([row[0].lower().capitalize(), int(row[2])])
-				total_men += int(row[2])
+    with open('names.csv', newline='') as women_csv:
+        reader = csv.reader(women_csv)
+        for row in reader:
+            if (row[1] == 'F'):
+                names_women.append([row[0].lower().capitalize(), int(row[2])])
+                total_women += int(row[2])
+            else:
+                names_men.append([row[0].lower().capitalize(), int(row[2])])
+                total_men += int(row[2])
 
-	# calculate weights 
-	for key, women in enumerate(names_women):
-		names_women[key][1] = 100 * names_women[key][1]/total_women
+    # calculate weights
+    for key, _ in enumerate(names_women):
+        names_women[key][1] = 100 * names_women[key][1]/total_women
 
-	for key, men in enumerate(names_men):
-		names_men[key][1] = 100 * names_men[key][1]/total_men
+    for key, _ in enumerate(names_men):
+        names_men[key][1] = 100 * names_men[key][1]/total_men
 
-	with open('names.json', 'w') as output:
-		json.dump({'male': names_men, 'female': names_women}, output, indent=4)
+    with open('names.json', 'w') as output:
+        json.dump({'male': names_men, 'female': names_women}, output, indent=4)
 
 if __name__ == '__main__':
-	main()
+    main()
--- trollfactory/props/langs/polish/car-list.py
+++ trollfactory/props/langs/polish/car-list.py
@@ -1,4 +1,4 @@
-import requests, json 
+import requests, json
 
 def find_between( s, first, last ):
     try:
@@ -9,84 +9,84 @@ def find_between( s, first, last ):
         return ""
 
 def main ():
-	r = requests.get("https://otomoto.pl")
+    r = requests.get("https://otomoto.pl")
 
-	try:
-		data = json.loads(find_between(find_between(r.text, '<script id="__NEXT_DATA__" type="application/json"', 'cript>'), '==">', '</s'))
-	except:
-		print('[!] failed to parse/find data!')
-		return 
+    try:
+        data = json.loads(find_between(find_between(r.text, '<script id="__NEXT_DATA__" type="application/json"', 'cript>'), '==">', '</s'))
+    except:
+        print('[!] failed to parse/find data!')
+        return
 
-	data = data['props']['pageProps']
+    data = data['props']['pageProps']
 
-	totalProducts = 0
+    totalProducts = 0
 
-	carBrandsOriginal = data['filters']['571']['_meta']['values'][1]['group_values']
-	carBrandsTransformed = []
-	
-	for carBrand in carBrandsOriginal: 
-		carBrandTransformed = {}
-		carBrandTransformed['value_key'] = carBrand['value_key']
+    carBrandsOriginal = data['filters']['571']['_meta']['values'][1]['group_values']
+    carBrandsTransformed = []
 
-		carBrandTransformed['name'] = carBrand['name'].split(' (')[0]
-		carBrandTransformed['products'] = int(carBrand['name'].split(' (')[1][:-1])
+    for carBrand in carBrandsOriginal:
+        carBrandTransformed = {}
+        carBrandTransformed['value_key'] = carBrand['value_key']
 
-		if (carBrandTransformed['products'] == 0):
-			continue
+        carBrandTransformed['name'] = carBrand['name'].split(' (')[0]
+        carBrandTransformed['products'] = int(carBrand['name'].split(' (')[1][:-1])
 
-		totalProducts += carBrandTransformed['products']
+        if (carBrandTransformed['products'] == 0):
+            continue
 
-		carBrandsTransformed.append(carBrandTransformed)
+        totalProducts += carBrandTransformed['products']
 
-	# lets calculate the brands weights now
-	for carBrandKey, carBrand in enumerate(carBrandsTransformed):
-		carBrandsTransformed[carBrandKey]['weight'] = ((carBrand['products'] / totalProducts) * 100)
+        carBrandsTransformed.append(carBrandTransformed)
 
-	# we have all brand's keys which we need to get models for each brand, so lets do it
+    # lets calculate the brands weights now
+    for carBrandKey, carBrand in enumerate(carBrandsTransformed):
+        carBrandsTransformed[carBrandKey]['weight'] = ((carBrand['products'] / totalProducts) * 100)
 
-	carModels = [] # key: brand name, value: car model 
+    # we have all brand's keys which we need to get models for each brand, so lets do it
 
-	for carBrand in carBrandsTransformed:
-		carModelsTemp = []
-		
-		brandModels = data['filtersValues']['573:571:' + carBrand['value_key']][0]['group_values'] 
-		for brandModel in brandModels:
-			carModel = {}
+    carModels = [] # key: brand name, value: car model
 
-			carModel['name'] = brandModel['name'].split(' (')[0]
-			carModel['products'] = int(brandModel['name'].rsplit(' (', 1)[1][:-1])
+    for carBrand in carBrandsTransformed:
+        carModelsTemp = []
 
-			if (carModel['products'] == 0):
-				continue
+        brandModels = data['filtersValues']['573:571:' + carBrand['value_key']][0]['group_values']
+        for brandModel in brandModels:
+            carModel = {}
 
-			carModel['weight'] = ((carModel['products'] / totalProducts) * 100) 
+            carModel['name'] = brandModel['name'].split(' (')[0]
+            carModel['products'] = int(brandModel['name'].rsplit(' (', 1)[1][:-1])
 
-			if '3018:573:' + brandModel['value_key'] in data['filtersValues']:
-				generations = [] 
-				for generation in data['filtersValues']['3018:573:' + brandModel['value_key']][0]['group_values']:
-					generationName = generation['name'].split(' (')[0]
-					generationProducts = int(generation['name'].rsplit(' (', 1)[1][:-1])
+            if (carModel['products'] == 0):
+                continue
 
-					if (generationProducts == 0):
-						continue 
+            carModel['weight'] = ((carModel['products'] / totalProducts) * 100)
 
-					generations.append({
-						'generation_name': generationName,
-						'generation_weight': ((generationProducts / totalProducts) * 100)
-					})
+            if '3018:573:' + brandModel['value_key'] in data['filtersValues']:
+                generations = []
+                for generation in data['filtersValues']['3018:573:' + brandModel['value_key']][0]['group_values']:
+                    generationName = generation['name'].split(' (')[0]
+                    generationProducts = int(generation['name'].rsplit(' (', 1)[1][:-1])
 
-				carModel['generations'] = generations
+                    if (generationProducts == 0):
+                        continue
 
-			carModelsTemp.append(carModel)
+                    generations.append({
+                        'generation_name': generationName,
+                        'generation_weight': ((generationProducts / totalProducts) * 100)
+                    })
 
-		carModels.append({
-			"brand_name": carBrand['name'],
-			"brand_weight": carBrand['weight'],
-			"models": carModelsTemp
-		})
+                carModel['generations'] = generations
 
-	with open('car-list.json', 'w', encoding='utf-8') as f:
-		json.dump(carModels, f, indent=4, ensure_ascii=False)
+            carModelsTemp.append(carModel)
+
+        carModels.append({
+            "brand_name": carBrand['name'],
+            "brand_weight": carBrand['weight'],
+            "models": carModelsTemp
+        })
+
+    with open('car-list.json', 'w', encoding='utf-8') as f:
+        json.dump(carModels, f, indent=4, ensure_ascii=False)
 
 if __name__ == '__main__':
-	main()
+    main()
