37793f8f5b03e60917134eb3d38e4588c6afb75a
0xA7404A <nyaurora@disroot.org>
2021-08-07 02:47:49 +0200

    Exclude props option

--- trollfactory/functions.py
+++ trollfactory/functions.py
@@ -1,8 +1,9 @@
 from trollfactory import props
 from trollfactory.props import *
+from trollfactory.exceptions import UnmetDependenciesException
 from sys import modules
 
-def generate_personality(p_dataset = 'polish', p_gender = 'female'):
+def generate_personality(p_dataset = 'polish', p_gender = 'female', exclude_props = []):
     properties_static = {
         'language': {
             'language': p_dataset
@@ -13,6 +14,9 @@ def generate_personality(p_dataset = 'polish', p_gender = 'female'):
     }
 
     properties = [i for i in props.__all__]
+
+    for prop in exclude_props:
+        properties.remove(prop)
 
     while len(properties) > 0:
         for prop_name in properties:
@@ -25,6 +29,10 @@ def generate_personality(p_dataset = 'polish', p_gender = 'female'):
                 for dependency in prop_class.dependencies:
                     if not dependency.lower() in properties_static.keys():
                         missing_dependencies = True
+                    if dependency not in properties:
+                        raise UnmetDependenciesException(
+                            'Unmet dependency: ' + dependency
+                        )
             if (missing_dependencies): continue
 
             prop_attrs = prop_class.generate(properties_static)
