ee4475a45e8cb0036fc2168ae632b513757e6648
0xA7404A <nyaurora@disroot.org>
2021-10-14 20:38:25 +0200

    Unsupported dataset/gender exceptions

--- trollfactory/exceptions.py
+++ trollfactory/exceptions.py
@@ -1,3 +1,13 @@
 class UnmetDependenciesException(Exception):
     def __init__(self, message):
         self.message = message
+
+
+class UnsupportedDatasetException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class InvalidGenderException(Exception):
+    def __init__(self, message):
+        self.message = message
