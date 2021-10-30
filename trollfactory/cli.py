f644d41e96867d998c37f7f398458fcd8f3cbb89
stanislawowski <office@stanislawowski.pl>
2021-10-30 14:42:20 +0200

    Bump version: 2.1.0 → 2.1.1

--- trollfactory/cli.py
+++ trollfactory/cli.py
@@ -5,7 +5,7 @@ from argparse import ArgumentParser, Namespace
 from datetime import datetime
 from trollfactory.functions import generate_personality
 
-TROLLFACTORY_VERSION: str = '2.1.0'
+TROLLFACTORY_VERSION: str = '2.1.1'
 DESCRIPTION: str = 'Fake personality generator for the 21st century!'
 
 
