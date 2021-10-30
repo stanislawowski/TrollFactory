a11000ef60b9d0d17eaca0eba99e98535a2a39ef
0xA7404A <nyaurora@disroot.org>
2021-10-30 14:42:20 +0200

    Bump version: 2.1.0 → 2.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-VERSION: str = '2.1.0'
+VERSION: str = '2.1.1'
 DESCRIPTION: str = 'Fake personality generator for the 21st century!'
 
 with open('README.md', encoding='utf-8') as file:
