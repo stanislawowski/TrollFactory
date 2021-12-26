83006ed5bf0ffea31f3e92bbd383df2b172dbbf3
0xA7404A <nyaurora@disroot.org>
2021-12-26 03:44:27 +0100

    Attributes list

--- trollfactory/props/__init__.py
+++ trollfactory/props/__init__.py
@@ -13,5 +13,11 @@ __props__: list[str] = [
 
 __all__ = __props__
 
+__attribs__: list[str] = []
+
 for prop in __props__:
-    import_module('trollfactory.props.'+prop)
+    prop_module = import_module('trollfactory.props.'+prop)
+    for attrib in getattr(prop_module, prop.capitalize()+'Type'
+                          ).__annotations__.keys():
+        if attrib != 'prop_title':
+            __attribs__.append(attrib)
